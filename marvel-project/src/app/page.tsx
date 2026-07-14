1c4ce71c69bce22662a34f715c6af137cd1c538c
unknown <lena5imova@gmail.com>
2026-07-14 18:08:32 +0500

    created Vision

--- marvel-project/src/app/page.tsx
+++ marvel-project/src/app/page.tsx
@@ -1,15 +1,17 @@
 import HeroDescriprion from "@/components/sections/HeroDescriprion/HeroDescriprion";
 import HeroesList from "@/components/sections/HeroesList/HeroesList";
 import RandomHero from "@/components/sections/RandomHero/RandomHero";
+import Vision from "@/components/sections/Vision/Vision";
 
 export default function Home() {
   return (
     <>
       <RandomHero/>
-      <div className="flex">
+      <div className="flex my-10">
         <HeroesList/>
         <HeroDescriprion/>
       </div>
+      <Vision/>
     </>
   )
 }
