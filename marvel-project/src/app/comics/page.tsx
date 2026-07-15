f20c1fd6fa70546d2c957477a812a1890d9f362a
unknown <lena5imova@gmail.com>
2026-07-15 13:13:36 +0500

    added ComicsCard component

--- marvel-project/src/app/comics/page.tsx
+++ marvel-project/src/app/comics/page.tsx
@@ -1,10 +1,13 @@
-
+import ComicsCard from "@/components/sections/ComicsCard/ComicsCard";
 import Banner from "@/components/ui/Banner/Banner";
+
+
 
 export default function Comics() {
   return (
     <>
       <Banner />
+      <ComicsCard />
     </>
   )
 }
