31108c5a20e499aba015d3948a9bb5a2721d5a5c
Lenabashimova <100222709+Lenabashimova@users.noreply.github.com>
2026-07-15 15:24:01 +0500

    Banner (#6)
    * added RandomHero section including ChangeHero and RandomHero components
    
    * replaced <a> with <Link>
    
    * created HeroList and HeroCard
    
    * replaced ListItem with HeroCard
    
    * created HeroDescription component
    
    * created Vision
    
    * added LoadMore button to Home page, created comics page and Banner component

--- marvel-project/src/app/comics/page.tsx
+++ marvel-project/src/app/comics/page.tsx
@@ -1,13 +1,10 @@
-import ComicsCard from "@/components/sections/ComicsCard/ComicsCard";
+
 import Banner from "@/components/ui/Banner/Banner";
-
-
 
 export default function Comics() {
   return (
     <>
       <Banner />
-      <ComicsCard />
     </>
   )
 }
