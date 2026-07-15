a9a16d766165db8ec3acdc73a675853b4a78e568
Lenabashimova <100222709+Lenabashimova@users.noreply.github.com>
2026-07-15 15:58:40 +0500

    fixed comics page problem (#9)

--- marvel-project/src/app/comics/page.tsx
+++ marvel-project/src/app/comics/page.tsx
@@ -1,11 +1,18 @@
-
 import Banner from "@/components/ui/Banner/Banner";
+import ComicsList from "@/components/sections/ComicsList/ComicsList";
+import Button from "@/components/ui/Button/Button";
+
 
 export default function Comics() {
   return (
     <>
     <div className="flex flex-col">
       <Banner />
+      <ComicsList/>
+      <div className="flex justify-center my-13">
+        <Button href="#" color='red'>LOAD MORE</Button> 
+      </div>
+    </div>
     </>
   )
 }
