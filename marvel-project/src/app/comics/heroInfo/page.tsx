e2f97e2e77de49a0fadcc056fc8d6537068bdaa6
Lenabashimova <100222709+Lenabashimova@users.noreply.github.com>
2026-07-22 12:23:45 +0500

    Hero info (#11)
    * fixed comics page problem
    
    * created comicsInfo page
    
    * created HeroInfo page

---
+++ marvel-project/src/app/comics/heroInfo/page.tsx
@@ -0,0 +1,20 @@
+
+
+
+
+
+import Banner from "@/components/ui/Banner/Banner";
+import HeroInfo from "@/components/sections/HeroInfo/HeroInfo";
+
+
+
+export default function Comics() {
+  return (
+    <>
+    <div className="flex flex-col gap-15">
+      <Banner />
+      <HeroInfo />
+    </div>
+    </>
+  )
+}
